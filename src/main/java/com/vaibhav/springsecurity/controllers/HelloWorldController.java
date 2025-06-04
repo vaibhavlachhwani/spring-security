f48f7466c5c7e1334700a815a61a9c7850322c33
vaibhavlachhwani <vaibhav.lachhwani111@gmail.com>
2025-06-04 21:25:09 +0530

    demonstrate default form based authentication

---
+++ src/main/java/com/vaibhav/springsecurity/controllers/HelloWorldController.java
@@ -0,0 +1,12 @@
+package com.vaibhav.springsecurity.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+    @GetMapping("/hello")
+    public String helloWorld() {
+        return "Hello, World!";
+    }
+}
