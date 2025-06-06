698fdcb31926821349305843aeb36512a0e8e91f
vaibhavlachhwani <vaibhav.lachhwani111@gmail.com>
2025-06-06 21:11:04 +0530

    implement post request with CSRF protection using csrf sync token pattern

---
+++ src/main/java/com/vaibhav/springsecurity/controllers/CsrfTokenController.java
@@ -0,0 +1,14 @@
+package com.vaibhav.springsecurity.controllers;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.security.web.csrf.CsrfToken;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CsrfTokenController {
+    @GetMapping("/csrf-token")
+    public CsrfToken getCsrfToken(HttpServletRequest request) {
+        return (CsrfToken) request.getAttribute("_csrf");
+    }
+}
