3468531f54844446a3d6b95003a6345e65a9d17a
vaibhavlachhwani <vaibhav.lachhwani111@gmail.com>
2025-06-09 18:13:37 +0530

    InMemory user credential storage

---
+++ src/main/java/com/vaibhav/springsecurity/entities/UserRoles.java
@@ -0,0 +1,6 @@
+package com.vaibhav.springsecurity.entities;
+
+public enum UserRoles {
+    ADMIN,
+    USER
+}
--- src/main/java/com/vaibhav/springsecurity/security/BasicAuthSecurityConfiguration.java
+++ src/main/java/com/vaibhav/springsecurity/security/BasicAuthSecurityConfiguration.java
@@ -1,10 +1,15 @@
 package com.vaibhav.springsecurity.security;
 
+import com.vaibhav.springsecurity.entities.UserRoles;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import java.net.http.HttpRequest;
@@ -13,13 +18,14 @@ import java.net.http.HttpRequest;
 public class BasicAuthSecurityConfiguration {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity request) throws Exception {
-        return request
+        request
                 .authorizeHttpRequests(
                         (auth) -> {
                             auth.anyRequest().authenticated();
                         }
                 )
                 .formLogin(form -> form.disable())
+                .logout(logout -> logout.disable())
                 .httpBasic(Customizer.withDefaults())
                 .sessionManagement(
                         (session) -> {
@@ -30,7 +36,23 @@ public class BasicAuthSecurityConfiguration {
                         (csrf) -> {
                             csrf.disable();
                         }
-                )
+                );
+
+        return request.build();
+    }
+
+    @Bean
+    public UserDetailsService users() {
+        UserDetails user = User.withUsername("user")
+                .password("{noop}test")
+                .roles(UserRoles.USER.name())
                 .build();
+
+        UserDetails admin = User.withUsername("admin")
+                .password("{noop}admin!23")
+                .roles(UserRoles.ADMIN.name())
+                .build();
+
+        return new InMemoryUserDetailsManager(user, admin);
     }
 }
